8be4241f117e67424ced6d24c05ae10cf9f8c8be
BrewLA <brewla.design@gmail.com>
2024-06-23 16:30:08 -0700

    fixed dependencies

---
+++ src/app/page-components/body/CardLink.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function CardLink() {
+    return (
+        <Link href="/components/accordion" className="flex flex-col gap-6 bg-primary border border-secondary pl-8 py-8 rounded-3xl">
+            <div className="flex flex-col">
+                <h2 className="text-lg font-normal">Explore our library of components.</h2>
+                <h2 className="text-lg font-normal text-secondary">Buttons. Cards. Forms. Icons. More.</h2>
+            </div>
+            <Image
+            src="/images/thumbnail-img.svg"
+            width={900}
+            height={600}
+            alt="Card link"
+            />
+        </Link>
+    );
+}
